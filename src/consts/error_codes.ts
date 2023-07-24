3728cc9259514e823cdc6b4940af5efda7959421
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-07-24 14:27:33 +0700

    Add error code consts

---
+++ src/consts/error_codes.ts
@@ -0,0 +1,15 @@
+namespace UserErrors {
+  export const ERR_NOT_FOUND = 100
+  export const ERR_RETRIEVE_FAILED = 101
+  export const ERR_CREATE_FAILED = 102
+  export const ERR_UPDATE_FAILED = 103
+  export const ERR_DELETE_FAILED = 104
+}
+
+namespace ArticleErrors {
+  export const ERR_NOT_FOUND = 200
+  export const ERR_RETRIEVE_FAILED = 201
+  export const ERR_CREATE_FAILED = 202
+  export const ERR_UPDATE_FAILED = 203
+  export const ERR_DELETE_FAILED = 204
+}
