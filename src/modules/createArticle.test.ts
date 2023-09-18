f1587723053707deb37926928ac38657bca90e87
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-09-18 07:11:09 +0700

    Refactor mocks

--- src/modules/createArticle.test.ts
+++ src/modules/createArticle.test.ts
@@ -1,23 +1,17 @@
 import { createArticle } from './createArticle'
-import { Account } from '../models'
 
 test('test func', () => {
-  jest.mock('../repositories/account', () => {
-    return jest.fn().mockImplementation(() => {
-      return {
-        create: jest.fn().mockImplementationOnce(() => Promise.resolve()),
-      }
-    })
-  })
+  jest.mock('../repositories/account', () =>
+    jest.fn().mockImplementation(() => ({
+      create: jest.fn(),
+    }))
+  )
 
-  const now = new Date().toISOString()
-  const newAccount: Account = {
+  createArticle({
     id: 1,
     username: 'fakeusername',
     fullName: 'Fake Fullname',
     email: 'fake@email.com',
-    createdAt: now,
-  }
-
-  createArticle(newAccount)
+    createdAt: new Date().toISOString(),
+  })
 })
